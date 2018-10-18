37902fd051361b61b0b066dcb7dd73c27d87a236
Jonas Almeida <jalmeida@mathbiol.org>
2018-10-18 11:53:32 -0400

    removing call log

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var root = { hello: () => 'Hello world!' };
 http.createServer(function (request, response) {
     graphql(schema, '{ hello }', root)
       .then((res) => {
-        console.log(response,res);
+        //console.log(response,res);
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.end(`querying for { hello } returned\n${JSON.stringify(res,null,3)}`);
     });      
