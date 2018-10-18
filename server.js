c22496ba0d62ed386e49040ca4bb2bd949738517
Jonas Almeida <jalmeida@mathbiol.org>
2018-10-18 11:45:48 -0400

    package for continuous deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // https://graphql.org/code/
 
 var http = require('http');
+var port = process.env.PORT || 4000
 var { graphql, buildSchema } = require('graphql');
 var schema = buildSchema(`
   type Query {
@@ -15,48 +16,8 @@ http.createServer(function (request, response) {
       .then((res) => {
         console.log(response,res);
         response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.end(`Hello at ${Date()} returned\n ${JSON.stringify(res,null,3)}`);
+        response.end(`querying for { hello } returned\n${JSON.stringify(res,null,3)}`);
     });      
-}).listen(4000);
+}).listen(port);
 
-console.log('Server started');
-
-
-
-/*
-
-/// WITH EXPRESS ///
-
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
-var cors = require('cors');
-
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
-
-// The root provides a resolver function for each API endpoint
-var root = {
-  hello: () => {
-    let r = 'Hello Earth'
-    return r 
-  },
-};
-
-var app = express();
-// CORS
-app.use(cors());
-
-// graphql middle layer
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
-app.listen(4000);
-console.log('Running a GraphQL API server at localhost:4000/graphql');
-*/
+console.log(`listening to ${port} at ${Date()}`);
