b55ad9f745462869da2a36bc0069783cea979cb1
Jonas Almeida <jalmeida@mathbiol.org>
2018-10-18 11:58:06 -0400

    protection from favicon call

--- server.js
+++ server.js
@@ -12,12 +12,17 @@ var schema = buildSchema(`
 var root = { hello: () => 'Hello world!' };
 
 http.createServer(function (request, response) {
+  if (request.url != '/favicon.ico') {
     graphql(schema, '{ hello }', root)
       .then((res) => {
         //console.log(response,res);
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.end(`querying for { hello } returned\n${JSON.stringify(res,null,3)}`);
-    });      
+    });
+  }else{
+    response.end(''); //<-- favicon being requested
+  }
+          
 }).listen(port);
 
 console.log(`listening to ${port} at ${Date()}`);
