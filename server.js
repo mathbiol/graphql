904d9fc7cffaa7f6e6284987bdb1670544964ad5
Jonas Almeida <jalmeida@mathbiol.org>
2018-10-25 14:03:44 -0400

    https://www.apollographql.com/docs/apollo-server/getting-started.html#Step-2-Install-dependencies

--- server.js
+++ server.js
@@ -1,5 +1,60 @@
 // https://graphql.org/code/
 
+const { ApolloServer, gql } = require('apollo-server');
+
+// This is a (sample) collection of books we'll be able to query
+// the GraphQL server for.  A more complete example might fetch
+// from an existing data source like a REST API or database.
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+// Type definitions define the "shape" of your data and specify
+// which ways the data can be fetched from the GraphQL server.
+const typeDefs = gql`
+  # Comments in GraphQL are defined with the hash (#) symbol.
+
+  # This "Book" type can be used in other type declarations.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is the root of all GraphQL queries.
+  # (A "Mutation" type will be covered later on.)
+  type Query {
+    books: [Book]
+  }
+`;
+
+// Resolvers define the technique for fetching the types in the
+// schema.  We'll retrieve books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+// In the most basic sense, the ApolloServer can be started
+// by passing type definitions (typeDefs) and the resolvers
+// responsible for fetching the data for those types.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// This `listen` method launches a web-server.  Existing apps
+// can utilize middleware options, which we'll discuss later.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
+
+
+/*
 var http = require('http');
 var port = process.env.PORT || 4000
 var { graphql, buildSchema } = require('graphql');
@@ -17,7 +72,7 @@ http.createServer(function (request, response) {
       .then((res) => {
         //console.log(response,res);
         response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.end(`querying for { hello } returned\n${JSON.stringify(res,null,3)}`);
+        response.end(`querying for { hello } returned\n${JSON.stringify(res,null,3)}\nsource code at https://www.github.com/mathbiol/graphql`);
     });
   }else{
     response.end(''); //<-- favicon being requested
@@ -26,3 +81,4 @@ http.createServer(function (request, response) {
 }).listen(port);
 
 console.log(`listening to ${port} at ${Date()}`);
+*/
